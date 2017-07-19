d8d700f57dd4aa67a840f5436d778b982a87ca45
Allison Wilbur <allison@saucelabs.com>
2017-07-18 22:55:50 -0700

    restrict bot to a channel

--- bot.js
+++ bot.js
@@ -1,26 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = process.env.DISCORD_TOKEN;
+const TOKEN = process.env.DISCORD_TOKEN;
+const BOT_CHANNEL = "professor-willow" // restrict bot usage to this channel
 
 // Load raw data
 var fs = require('fs');
 var counters = JSON.parse(fs.readFileSync("counters.json"));
 var cp = JSON.parse(fs.readFileSync("cp.json"));
 
-// Function that capitalizes the first word of input, for display purposes
+// ** Helper functions: **
+// Capitalizes the first word of input, for display purposes
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
+function appropriateChannel(message) {
+    if (message.channel.name != BOT_CHANNEL) {
+        message.reply("Please use this command in "+BOT_CHANNEL+" instead, thanks!");
+        return false;
+    }
+    return true;
+}
+
 // Returns a String list of recommended counters for the given Pokemon
 function getCounters(pokemon) {
-    var counterList;
-    try {
-        counterList = counters[pokemon];
-    }
-    catch(e) {
-        return "Sorry, counters for "+pokemon.capitalize()+" aren't availabe at this time"
+    var counterList = counters[pokemon];
+    if (counterList == null) {
+        return "Sorry, counters for "+pokemon.capitalize()+" aren't availabe at this time";
     }
     var reply = "";
     for (var i=0; i<counterList.length; i++) {
@@ -50,12 +57,18 @@ client.on('ready', () => {
 client.on('message', message => {
     try {
         if (message.content.toLowerCase().lastIndexOf('!cp', 0) === 0) {
+            if (!appropriateChannel(message)) {
+                return;
+            }
             var pokemon = message.content.split(" ")[1].toLowerCase();
             var reply = getCP(pokemon);
             message.channel.send(reply);
         }
 
         if (message.content.toLowerCase().lastIndexOf('!counter', 0) === 0) {
+            if (!appropriateChannel(message)) {
+                return;
+            }
             var pokemon = message.content.split(" ")[1].toLowerCase();
             var reply = getCounters(pokemon);
             message.channel.send(reply);
@@ -67,4 +80,4 @@ client.on('message', message => {
     }
 });
 
-client.login(token);
+client.login(TOKEN);
